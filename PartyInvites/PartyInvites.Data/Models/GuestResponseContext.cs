b91253a4e24394fdfe3c6237119906b58d65de91
Daniel Larner <dlarner3194@gmail.com>
2018-05-06 13:00:23 -0700

    add DI for dbcontext

---
+++ PartyInvites/PartyInvites.Data/Models/GuestResponseContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartyInvites.Data.Models
+{
+  public class GuestResponseContext : DbContext
+  {
+    public GuestResponseContext(DbContextOptions<GuestResponseContext> options)
+      : base(options)
+    {
+    }
+
+    public DbSet<GuestResponse> GuestResponses { get; set; }
+  }
+}
--- PartyInvites/PartyInvites.Data/Startup.cs
+++ PartyInvites/PartyInvites.Data/Startup.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using PartyInvites.Data.Models;
 
 namespace PartyInvites.Data
 {
@@ -23,6 +25,8 @@ namespace PartyInvites.Data
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<GuestResponseContext>(opt =>
+                opt.UseInMemoryDatabase("GuestList"));
             services.AddMvc();
         }
 
